090fcd0ea3d75cc9ba17a154e51c3e9fbc087bf7
Vipul Raj <38418118+vipulraj01@users.noreply.github.com>
2023-07-29 10:06:11 +0530

    Add files via upload

---
+++ C++/sum_of_two.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+int a,b,c;
+cout<<"Enter first number ";
+cin>>a;
+cout<<"\nEnter second number ";
+cin>>b;
+c = a+b;
+cout<<"The sum of "<<a<< " and "<<b<<" = "<<c;
+}
