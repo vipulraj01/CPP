e1a9d078a3a282b140a5bee45d5641d1c77a0015
Vipul Raj <38418118+vipulraj01@users.noreply.github.com>
2023-07-31 10:06:45 +0530

    Add files via upload

---
+++ C++/data_types.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+int a = 5;
+char b = 'c';
+float c = 3.4;
+double d = 3.1456789;
+bool e = true;
+
+cout<<"The value of int is "<<a<< "\nThe value of char is "<<b<< "\nThe value of float is "<<c<< "\nThe value of double is "<<d<< "\nThe value of bool is "<<e;
+}
